499c0a71e563edff307f69f0440d545d47e798e0
Cameron Wood <cascwo@gmail.com>
2023-12-17 23:29:01 -0700

    The initial working script

---
+++ sntocsv.py
@@ -0,0 +1,10 @@
+import sys, os
+
+with open('out_file.csv', 'w') as out_file:
+	with open('in_file.txt', 'r') as in_file:
+		for line in in_file:
+			if 'Serial No: ' in line:
+				str1 = line.split('Serial No:',1)[1]
+				str2 = str1.strip
+				finalstr = str2()[:15]
+				out_file.write(finalstr + '\n')
